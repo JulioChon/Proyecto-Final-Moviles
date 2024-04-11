48a320d01ef054bf54f39f341a4c578e2f33600b
Di1az <daniel271202@gmail.com>
2024-04-10 20:15:57 -0700

    Agrega fotos al contenedor, falta configurar el tamaño

--- app/src/main/java/itson/traveldiary/CreateAlbumActivity.kt
+++ app/src/main/java/itson/traveldiary/CreateAlbumActivity.kt
@@ -1,17 +1,31 @@
 package itson.traveldiary
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 
 class CreateAlbumActivity : AppCompatActivity() {
 
     private lateinit var planificationContainer: LinearLayout
+
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 1001
+        const val REQUEST_CODE_GALLERY = 1002
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +41,12 @@ class CreateAlbumActivity : AppCompatActivity() {
         val botonAgregarPlanificacion = findViewById<Button>(R.id.boton_agregar_planificacion)
         botonAgregarPlanificacion.setOnClickListener {
             addEditText()
+        }
+
+
+        val botonAgregarFotos = findViewById<ImageButton>(R.id.boton_agregar_fotos)
+        botonAgregarFotos.setOnClickListener {
+            checkGalleryPermissionAndOpen()
         }
     }
     private fun addEditText() {
@@ -44,4 +64,56 @@ class CreateAlbumActivity : AppCompatActivity() {
         planificationContainer.addView(editText)
     }
 
+    private fun checkGalleryPermissionAndOpen() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, REQUEST_CODE_GALLERY)
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_CODE_GALLERY)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery()
+                } else {
+                    // msj error o algo asi
+                }
+                return
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_GALLERY && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri = data.data
+            agregarImagenAlGridView(imageUri)
+        }
+    }
+
+    private fun agregarImagenAlGridView(imageUri: Uri?) {
+        val imageView = ImageView(this).apply {
+            setImageURI(imageUri)
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        }
+        val contenedorFotos = findViewById<LinearLayout>(R.id.contenedor_fotos)
+        contenedorFotos.addView(imageView)
+    }
 }
+
+
